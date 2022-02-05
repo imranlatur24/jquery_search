71eccbfb88ac83483f2c101c762a1ff880d528d7
imran <imranlatur24@gmail.com>
2022-02-05 12:53:39 +0530

    done changes

--- app/models.py
+++ app/models.py
@@ -5,9 +5,13 @@ class CityModel(models.Model):
     city=models.CharField(max_length=25)
     class Meta:
         db_table='search_city'
+    def __str__(self) -> str:
+        return self.city
 
 class CityCountry(models.Model):
     city=models.CharField(max_length=25)
     country=models.CharField(max_length=25)
     class Meta:
         db_table='search_city_country'
+    def __str__(self) -> str:
+        return self.city and self.country
